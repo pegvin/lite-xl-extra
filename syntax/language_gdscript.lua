ea5de4d3989f03252b0c87f182aab440c71108d7
ADITYA MISHRA <75035219+DEVLOPRR@users.noreply.github.com>
2022-04-05 17:05:08 +0530

    Update language_gdscript.lua

--- syntax/language_gdscript.lua
+++ syntax/language_gdscript.lua
@@ -4,25 +4,46 @@ local syntax = require "core.syntax"
 syntax.add {
 	name = "GD Script",
 	files = { "%.gd$" },
+
+	-- Character to append when pressing the shortcut key for adding comments
 	comment = "#",
 	patterns = {
-		{ pattern = "#.-\n",                  type = "comment"  },
-		{ pattern = { "\"\"\"", "%\"\"\"" },  type = "comment"  },
-		{ pattern = { '"', '"', '\\' },       type = "string"   },
-		{ pattern = { "`", "`", '\\' },       type = "string"   },
-		{ pattern = { "'", "'", '\\' },       type = "string"   },
-		{ pattern = "0[oO_][0-7]+",           type = "number"   },
-		{ pattern = "-?0x[%x_]+",             type = "number"   },
-		{ pattern = "-?%d+_%d",               type = "number"   },
-		{ pattern = "-?%d+[%d%.eE]*f?",       type = "number"   },
-		{ pattern = "-?%.?%d+f?",             type = "number"   },
-		{ pattern = "[%+%-=/%*%^%%<>!~|&]",   type = "operator" },
-		{ pattern = ":=",                     type = "operator" },
-		{ pattern = "[%a_][%w_]*%f[(]",       type = "function" },
-		{ pattern = "[%a_][%w_]*",            type = "symbol"   },
-		{ pattern = "{%a_}{%w_}*",            type = "symbol"   },
+		{ pattern = "#.-\n",                      type = "comment"  }, -- Single Line Comment
+		{ pattern = { "\"\"\"", "%\"\"\"" },      type = "comment"  }, -- Multiline Comments
+		{ pattern = { "%s\"\"\"", "%s?\"\"\"" },  type = "string"   }, -- Multiline String
+		{ pattern = { '"', '"', '\\' },           type = "string"   }, -- Normal Strings
+		{ pattern = "0x[0-9a-z]",                 type = "number"   }, -- HexaDecimal
+		{ pattern = "0b[0-1]",                    type = "number"   }, -- Binary
+		{ pattern = "-?%d+[%d%.eE]*f?",           type = "number"   },
+
+		-- Operators + - = / % *
+		{ pattern = "[%+ %- = / %* %%]",          type = "operator" },
+
+		-- Conditionals "!" "||" "&&"
+		-- Comparison "==" "!=" "<" ">" "=<" ">="
+		-- Bit shifting "<<" ">>"
+		-- Bitwise AND "&"
+		-- Bitwise XOR "^"
+		-- Bitwise OR "|"
+		-- Bitwise NOT "~"
+		{ pattern = "[%^%%<>!~|&]",               type = "operator" },
+
+		-- Type Inferfence Operator
+		{ pattern = ":=",                         type = "operator" },
+
+		-- Function Names With a-z, A-Z & _ In them
+		{ pattern = "[%a_][%w_]*%f[(]",           type = "function" },
+
+		-- Variable/Symbol Names With a-z, A-Z & _ In them
+		{ pattern = "[%a_][%w_]*",                type = "symbol"   },
 	},
 	symbols = {
+		-- Keywords
+		["is"]          = "keyword",
+		["as"]          = "keyword",
+		["in"]          = "keyword",
+		["not"]         = "keyword",
+		["and"]         = "keyword",
 		["if"]          = "keyword",
 		["else"]        = "keyword",
 		["elif"]        = "keyword",
@@ -59,10 +80,18 @@ syntax.add {
 		["mastersync"]  = "keyword",
 		["puppetsync"]  = "keyword",
 		["class_name"]  = "keyword2",
+
+		-- Constants
 		["TAU"]         = "keyword2",
 		["INF"]         = "keyword2",
 		["NAN"]         = "keyword2",
 		["PI"]          = "keyword2",
+
+		-- Types
+		["null"]        = "keyword2",
+		["bool"]        = "keyword2",
+		["int"]         = "keyword2",
+		["float"]       = "keyword2",
+		["String"]      = "keyword2",
 	},
 }
-
